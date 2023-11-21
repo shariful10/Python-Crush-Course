123af3fac8e44af531823fa02e479f4901411e47
Sk Shariful Islam <freelancershariful10@gmail.com>
2023-11-22 02:46:57 +0600

    Arithmetic operator

---
+++ 03_operator.py
@@ -0,0 +1,13 @@
+# Chapter 3. Operator in Python
+5 + 10
+
+# Type of operator
+
+# Arithmetic operator
+print(10 + 20) # Addition
+print(10 - 20) # Subtraction
+print(10 * 20) # Multiplication
+print(12 / 5) # Divition
+print(10 ** 2) # Exponentiation
+print(10 % 20) # Remainder
+print(12 // 5) # Floor Divition
