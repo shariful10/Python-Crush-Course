59730693a019a665418df6f8d43b3a979dd16c8a
Sk Shariful Islam <freelancershariful10@gmail.com>
2023-11-22 02:21:57 +0600

    Variable Datatype

---
+++ 02_variable_datatype.py
@@ -0,0 +1,39 @@
+# String type
+
+fruits = "mangos"
+fruits = "apples"
+print("01.", fruits)
+
+# Rules for naming variables
+
+# Right way
+# fruits = "mangos"
+# myfav_fruits = "mangos"
+# _fruits = "mangos"
+# fruits1 = "mangos"
+
+# Wrong way
+# $fruits = "mangos"
+# myfav fruits = "mangos"
+# 1fruits = "mangos"
+# fru-its = "mangos"
+
+# Data types in the Python
+# 1. number 2. string 3. boolean 4. list 5. set 6. dictionary
+
+# 1. number data type - integer and floating point
+
+# integer - 10, 20, 25, 125
+x = 21
+print(x)
+
+# floating point - 10.2, 20.5, 25.6, 12.5
+y = 10.5
+print(y)
+
+# boolean - True or False
+
+z = True
+f = False
+print(z)
+print(f)
